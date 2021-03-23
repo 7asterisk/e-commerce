b57dc9b4c27d52138b86ef029964a890c08c52e7
Mr.kumar <kumar@Akanshas-MacBook-Air.local>
2021-03-23 16:37:06 +0530

    second commit

--- src/app/pages/store/products-details/products-details.component.spec.ts
+++ src/app/pages/store/products-details/products-details.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProductsDetailsComponent } from './products-details.component';
 
@@ -6,7 +6,7 @@ describe('ProductsDetailsComponent', () => {
   let component: ProductsDetailsComponent;
   let fixture: ComponentFixture<ProductsDetailsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProductsDetailsComponent ]
     })
